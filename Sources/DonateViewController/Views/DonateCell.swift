00dd5e4c31e5a184b638b4c8726c5bf9bcb0a953
Nathan Fallet <contact@nathanfallet.me>
2020-06-22 13:07:21 +0200

    Shit

--- Sources/DonateViewController/Views/DonateCell.swift
+++ Sources/DonateViewController/Views/DonateCell.swift
@@ -20,7 +20,7 @@
 import Foundation
 import UIKit
 
-public class DonateCell: UITableViewCell {
+open class DonateCell: UITableViewCell {
     
     public let loading = UIActivityIndicatorView()
     
